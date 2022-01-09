8fdb5ee35f563cc19725d1dc83dbed10122f84a5
Grace <graceanneu@gmail.com>
2022-01-09 16:13:46 -0500

    finished project code

--- README.md
+++ README.md
@@ -1,2 +1,8 @@
 # rock-paper-scissors
 TOP rock, paper, scissors JS program
+
+functions:
+humanPlay - creates pop-up window to prompt human player, returns 'play' - a number 0-2 representing RPS
+computerPlay - chooses a random integer b/w 0-2
+playRound - says the outcome of a match
+game** - should be able to play multiple rounds of the game
--- rockPaperScissors.js
+++ rockPaperScissors.js
@@ -1,59 +1,108 @@
 //prompt player to choose 'rock', 'paper', or 'scissors'
-let play = prompt("Play 'rock', 'paper', or 'scissors'.")
-if (play.toLowerCase() == "rock") {
-    let rock = 0;
-}
-else if (play.toLowerCase() == "paper") {
-    let paper = 1;
-}
-else if (play.toLowerCase() == "scissors") {
-    let scissors = 2;
-}
 
-//define constants for game play function
-const playerSelection = play;
-const computerSelection = computerPlay();
+function humanPlay () {
+    let playerChoice = prompt("Play 'rock', 'paper', or 'scissors'.");
+    if (playerChoice.toLowerCase() === "rock") {
+        play = 0;
+        console.log("Your play: rock")
+    }
+    else if (playerChoice.toLowerCase() === "paper") {
+        play = 1;
+        console.log("Your play: paper")
+    }
+    else if (playerChoice.toLowerCase() === "scissors") {
+        play = 2;
+        console.log("Your play: scissors")
+    }
+    else {
+        alert("Refresh page and choose 'rock', 'paper', or 'scissors'.");
+    }
+    return play;
+}
+//humanPlay();
 
 //make computer give random output between 0 and 2
 function computerPlay(maximum) {
-    const number = (Math.random()*3)
-    const integer = (Math.floor(number))
-    return integer;
-}
+    const number = (Math.random()*3);
+    const integer = (Math.floor(number));
 
-//assign 0-2 to "rock", "paper", and "scissors"
-if ((computerPlay()) === 0) {
+if (integer === 0) {
     console.log("Computer's play: rock");
-} else if ((computerPlay()) === 1) {
+} else if (integer === 1) {
     console.log("Computer's play: paper");
-} else {
+} else if (integer === 2) {
     console.log("Computer's play: scissors");
 }
-
+return integer;
+}
 
 //program the winning and losing combinations
 function playRound(playerSelection, computerSelection) {
-    if (playerSelection === 0 && computerSelection === 0) {
-        console.log("It's a tie!");
-    } else if (playerSelection === 0 && computerSelection === 1) {
-        console.log("You lose! Paper beats rock.");
-    } else if (playerSelection === 0 && computerSelection === 2) {
-        console.log("You win! Rock beats scissors.");
-    } else if (playerSelection === 1 && computerSelection === 0) {
-        console.log("You win! Paper beats rock.");
-    } else if (playerSelection === 1 && computerSelection === 1) {
-        console.log("It's a tie!");
-    } else if (playerSelection === 1 && computerSelection === 2) {
-        console.log("You lose! Scissors beats paper.")
-    } else if (playerSelection === 2 && computerSelection === 0) {
-        console.log("You lose! Rock beats scissors.");
-    } else if (playerSelection === 2 && computerSelection === 1) {
-        console.log("You win! Paper beats rock.");
-    } else if (playerSelection === 2 && computerSelection === 2) {
-        console.log("It's a tie!")
+    if (playerSelection === computerSelection) {
+        console.log('Your score: ' + (playerScore));
+        console.log('Computer score: ' + (computerScore));
+    } else if (playerSelection == 0 && computerSelection == 1) {
+        computerScore = computerScore + 1;
+        console.log('Computer score: ' + (computerScore));
+        console.log('Your score: ' + (playerScore));
+    } else if (playerSelection == 0 && computerSelection == 2) {
+        playerScore = playerScore + 1;
+        console.log('Your score: ' + (playerScore));
+        console.log('Computer score: ' + (computerScore));
+    } else if (playerSelection == 1 && computerSelection == 0) {
+        playerScore = playerScore + 1;
+        console.log('Your score: ' + (playerScore));
+        console.log('Computer score: ' + (computerScore));
+    } else if (playerSelection == 1 && computerSelection == 2) {
+        computerScore = computerScore + 1;
+        console.log('Computer score: ' + (computerScore));
+        console.log('Your score: ' + (playerScore));
+    } else if (playerSelection == 2 && computerSelection == 0) {
+        computerScore = computerScore + 1;
+        console.log('Computer score: ' + (computerScore));
+        console.log('Your score: ' + (playerScore));   
+    } else if (playerSelection == 2 && computerSelection == 1) {
+        playerScore = playerScore + 1;
+        console.log('Your score: ' + (playerScore));
+        console.log('Computer score: ' + (computerScore));
     }
+
 }
 
-//function for a single round of rock, paper, scissors
-console.log(playRound(playerSelection, computerSelection));
 
+//define constants for game play function
+let playerSelection = "";
+let computerSelection = "";
+let computerScore = 0;
+let playerScore = 0;
+
+
+//console.log(playRound(playerSelection, computerSelection));
+//function for a single round of rock, paper, scissors
+function game() {
+    playerSelection = humanPlay ();
+    computerSelection = computerPlay ();
+    playRound(playerSelection, computerSelection);
+    playerSelection = humanPlay ();
+    computerSelection = computerPlay ();
+    playRound(playerSelection, computerSelection);
+    playerSelection = humanPlay ();
+    computerSelection = computerPlay ();
+    playRound(playerSelection, computerSelection);
+    playerSelection = humanPlay ();
+    computerSelection = computerPlay ();
+    playRound(playerSelection, computerSelection);
+    playerSelection = humanPlay ();
+    computerSelection = computerPlay ();
+    playRound(playerSelection, computerSelection);
+}
+
+console.log(game(computerScore, playerScore));
+
+if (computerScore > playerScore) {
+    console.log("Boohoo, you lose.")
+}else if (playerScore > computerScore) {
+    console.log("Congrats! You win!")
+}else if (playerScore == computerScore) {
+    console.log("It's a tie.")
+}
